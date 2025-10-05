46e4ef1bac7d814ba42468eb1cc06059ad8f487d
Punisher <palekseev102@gmail.com>
2025-10-05 13:55:33 +0300

    commit

--- Controllers/controllers.go
+++ Controllers/controllers.go
@@ -8,9 +8,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-var apiKey = "783e0858-39de-4c83-a72c-bc2858c795be"
-var layout string = ""
-
 func RequestFromIlya(c *gin.Context) {
 	var requestFromIlya models.RequestData
 	if err := c.ShouldBindJSON(&requestFromIlya); err != nil {
